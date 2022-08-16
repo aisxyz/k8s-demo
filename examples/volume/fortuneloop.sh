73cfb9415710ad502a29247d5a751cc85d7cf7f5
aisxyz <1191182126@qq.com>
2022-08-16 08:10:54 +0800

    add volume examples

--- examples/volume/fortuneloop.sh
+++ examples/volume/fortuneloop.sh
@@ -2,10 +2,12 @@
 
 trap "exit" SIGINT
 mkdir /var/htdocs
+#interval=${1:-5}
+interval=${INTERVAL:-5}
 
 while :
 do
-	echo $(date) Writing fortune to /var/htdocs/index.html
+	echo $(date +"%Y-%m-%d %H:%M:%S") Writing fortune to /var/htdocs/index.html
 	/games/fortune > /var/htdocs/index.html
-	sleep 10
+	sleep $interval
 done
